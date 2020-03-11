18b6062d75ee1c19fe382312d2ee406460e856fb
Artem Yakovlev <13.zrka@gmail.com>
2020-03-11 16:28:05 +0300

    fet(app): save fragments state

---
+++ app/src/main/java/com/example/wholesalemartdatabase/ui/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.wholesalemartdatabase.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.example.wholesalemartdatabase.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var currentNavController: LiveData<NavController>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState!!)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+
+        val navGraphIds = listOf(R.navigation.allview, R.navigation.search,
+                R.navigation.database_intents)
+
+        currentNavController = bottom_nav.setupWithNavController(
+                navGraphIds = navGraphIds,
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.nav_host_container,
+                intent = intent
+        )
+    }
+}
